78bc9c556389efa9a61d5610204d61781c829c54
Turan Ketene <turank97@gmail.com>
2019-12-16 21:51:09 -0800

    deployment code

---
+++ build/precache-manifest.849dce21fd48271c2f10eeabcb4413d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55618bd1086fbe8581a85f89e249a76f",
+    "url": "/Click-Game/index.html"
+  },
+  {
+    "revision": "e6b5ba6b15a408406768",
+    "url": "/Click-Game/static/css/main.31bb93c8.chunk.css"
+  },
+  {
+    "revision": "c43780d072d862424898",
+    "url": "/Click-Game/static/js/2.10ea4d50.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Click-Game/static/js/2.10ea4d50.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e6b5ba6b15a408406768",
+    "url": "/Click-Game/static/js/main.f6718d71.chunk.js"
+  },
+  {
+    "revision": "e9c1251e8846a0d896e6",
+    "url": "/Click-Game/static/js/runtime-main.22587c3a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Click-Game/static/media/logo.5d5d9eef.svg"
+  }
+]);
